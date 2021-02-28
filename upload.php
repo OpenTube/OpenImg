6f3a6df9d5be2073ab34938bc64ce1b2d7e4796b
ChillerDragon <ChillerDragon@gmail.com>
2021-02-28 15:17:12 +0100

    Send proper html skeleton

--- upload.php
+++ upload.php
@@ -1,4 +1,12 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
 <?php
 
 const MAX_FILE_SIZE = 5000000;
@@ -65,5 +73,6 @@ function upload_image() {
 upload_image();
 
 ?>
-
-<a href="index.php">back</a>
+    <a href="index.php">back</a>
+</body>
+</html>
